ce8da916a617afdd3b6be36f5ec6a2017f5f1ccb
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2024-07-31 03:38:03 +0530

    up-3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import LoginForm from './Components/LoginForm/LoginForm';
+import { supabase } from './createClient';
 function App() {
   return (
     <div className="App">
---
+++ src/createClient.js
@@ -0,0 +1,3 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://uvkngukzgufomfnikbtb.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2a25ndWt6Z3Vmb21mbmlrYnRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzNjkzMDMsImV4cCI6MjAzNzk0NTMwM30.SMpnrH13jArMExV3Mxm46oO5TgRrZF6Kbr2Q6361qyo")
