d45d1b0d579318c018015afc27d5cceb11212d18
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2024-07-31 03:52:32 +0530

    up-4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+
+import React,{useState, useEffect} from 'react';
 import './App.css';
 import LoginForm from './Components/LoginForm/LoginForm';
 import { supabase } from './createClient';
 function App() {
+
+  const [users,setUsers] = useState([])
+  console.log(users)
+
+  useEffect(() => {
+    FetchUsers()
+  }, [])  
+
+
+  async function FetchUsers(){
+    const {data} = await supabase
+    .from('users')
+    .select('*')
+    setUsers(data)
+    console.log(data)
+  }
+
   return (
     <div className="App">
       <LoginForm/>
